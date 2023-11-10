7082e80a97999facffa4213f39bae272b7af4906
MoBosse <50657629+MoBosse@users.noreply.github.com>
2023-11-10 14:06:10 +0000

    Deploying to gh-pages from  @ e46b2386e47776e0fa87f27271f287651921fcf1 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4619,7 +4619,7 @@ function alterDataDisplay(value, info, context, additionalInfo) {
     }
     if (obsAttributes.length > 0) {
         var obsAttributeFootnoteNumbers = obsAttributes.map(function(obsAttribute) {
-            return getObservationAttributeFootnoteSymbol(obsAttribute.value);
+            return getObservationAttributeFootnoteSymbol(obsAttribute.value, context);
         });
         altered += ' ' + obsAttributeFootnoteNumbers.join(' ');
     }
@@ -4632,8 +4632,13 @@ function alterDataDisplay(value, info, context, additionalInfo) {
  * @param {int} num
  * @returns {string} Number converted into unicode character for footnotes.
  */
-function getObservationAttributeFootnoteSymbol(val) {
-    return '<small> ' + val + '</small>';
+function getObservationAttributeFootnoteSymbol(value, context) {
+  if (context === 'chart tooltip' or context === 'table cell'){
+    return &euro; + value;
+  }
+  else{
+    return '<small> ' + translations.t(value) + '</small>';
+  }
 }
 
   /**
