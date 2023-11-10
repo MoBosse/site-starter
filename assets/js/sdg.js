db9e3616e5a5e29898ccc651fd393da6ee4669ea
MoBosse <50657629+MoBosse@users.noreply.github.com>
2023-11-10 13:38:40 +0000

    Deploying to gh-pages from  @ 387aeaa739721bf502491dd4298e69333c5301b7 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4633,7 +4633,7 @@ function alterDataDisplay(value, info, context, additionalInfo) {
  * @returns {string} Number converted into unicode character for footnotes.
  */
 function getObservationAttributeFootnoteSymbol(num) {
-    return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
+    return '[Test ' + (num + 1) + ']';
 }
 
   /**
