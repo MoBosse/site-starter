7a8557300c0303a5c67b2df955a621cccedeee77
Moritz Bosse <moritz.bosse@destatis.de>
2023-11-10 14:41:51 +0100

    2.4

--- _includes/assets/js/view/dataHelpers.js
+++ _includes/assets/js/view/dataHelpers.js
@@ -73,5 +73,5 @@ function alterDataDisplay(value, info, context, additionalInfo) {
  * @returns {string} Number converted into unicode character for footnotes.
  */
 function getObservationAttributeFootnoteSymbol(attributeValue) {
-    return '[Test ' + attributeValue + ']';
+    return '<small> ' + attributeValue + '</small>';
 }
